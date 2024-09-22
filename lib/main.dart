dae861ec79e6833a6e3c0ad66c556f924f9b458b
Ruslan <rsekirin@prendehealth.com>
2024-09-23 00:37:30 +0300

    Add localization files

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kopiyka/providers/theme_provider.dart';
+import 'package:kopiyka/providers/language_provider.dart';
 import 'package:kopiyka/routes.dart';
 import 'package:kopiyka/theme/app_theme.dart';
 
 void main() {
   runApp(
     ProviderScope(
-      child: MyApp(),
+      child: KopiykaApp(),
     ),
   );
 }
 
-class MyApp extends ConsumerWidget {
+class KopiykaApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeProvider);
-
+    final locale = ref.watch(languageProvider);
     return MaterialApp.router(
-      title: 'Flutter Theme Example',
+      title: 'Kopiyka',
       themeMode: themeMode,
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('uk', ''),
+      ],
       darkTheme: AppTheme.darkTheme,
       routerConfig: router,
+      locale: locale,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
     );
   }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kopiyka/providers/language_provider.dart';
 import 'package:kopiyka/providers/theme_provider.dart';
-import 'package:kopiyka/screens/dashboard/components/custom_drawer.dart';
 
 class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
@@ -9,40 +9,69 @@ class SettingsScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeProvider);
+    final selectedLanguage = ref.watch(languageProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
+          //title: const Text(AppLocalizations.of(context)!.lightTheme),
+          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Theme Settings',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            RadioListTile<ThemeMode>(
+              //title: Text(AppLocalizations.of(context)!.lightTheme),
+              value: ThemeMode.light,
+              groupValue: themeMode,
+              onChanged: (mode) {
+                ref.read(themeProvider.notifier).setLightMode();
+              },
+            ),
+            RadioListTile<ThemeMode>(
+              title: const Text('Dark Theme'),
+              value: ThemeMode.dark,
+              groupValue: themeMode,
+              onChanged: (mode) {
+                ref.read(themeProvider.notifier).setDarkMode();
+              },
+            ),
+            RadioListTile<ThemeMode>(
+              title: const Text('System Default'),
+              value: ThemeMode.system,
+              groupValue: themeMode,
+              onChanged: (mode) {
+                ref.read(themeProvider.notifier).setSystemMode();
+              },
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Language Settings',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            RadioListTile<Locale>(
+              title: const Text('English'),
+              value: const Locale('en'),
+              groupValue: selectedLanguage,
+              onChanged: (locale) {
+                ref.read(languageProvider.notifier).state = locale!;
+              },
+            ),
+            RadioListTile<Locale>(
+              title: const Text('Ukrainian'),
+              value: const Locale('uk'),
+              groupValue: selectedLanguage,
+              onChanged: (locale) {
+                ref.read(languageProvider.notifier).state = locale!;
+              },
+            ),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          RadioListTile<ThemeMode>(
-            title: const Text('Light Theme'),
-            value: ThemeMode.light,
-            groupValue: themeMode,
-            onChanged: (mode) {
-              ref.read(themeProvider.notifier).setLightMode();
-            },
-          ),
-          RadioListTile<ThemeMode>(
-            title: const Text('Dark Theme'),
-            value: ThemeMode.dark,
-            groupValue: themeMode,
-            onChanged: (mode) {
-              ref.read(themeProvider.notifier).setDarkMode();
-            },
-          ),
-          RadioListTile<ThemeMode>(
-            title: const Text('System Default'),
-            value: ThemeMode.system,
-            groupValue: themeMode,
-            onChanged: (mode) {
-              ref.read(themeProvider.notifier).setSystemMode();
-            },
-          ),
-        ],
-      ),
-      drawer: const CustomDrawer(),
     );
   }
 }
